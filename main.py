f001933bae0dceca088cf1f24f9a24da1024812b
Harsh <masterbei75@gmail.com>
2026-01-11 21:46:10 +0530

    actually making the file organizer

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
 import os
 import shutil
+
+path = input("Enter the path of the Folder you wanna organize : ").strip()
+
+FILE_TYPES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Videos": [".mp4", ".mkv", ".avi"],
+    "Documents": [".pdf", ".docx", ".txt", ".pptx"],
+    "Music": [".mp3", ".wav"],
+    "Archives": [".zip", ".rar", ".tar"],
+    "Code": [".py", ".js", ".cpp", ".java"]
+}
+
+for folder in FILE_TYPES:
+    os.makedirs(os.path.join(path, folder), exist_ok=True)
+
+os.makedirs(os.path.join(path, "Others"), exist_ok=True)
+
+for item in os.listdir(path):
+    item_path = os.path.join(path, item)
+
+    if not os.path.isfile(item_path):
+        continue
+
+    ext = os.path.splitext(item)[1].lower()
+    moved = False
+
+    for folder, extensions in FILE_TYPES.items():
+        if ext in extensions:
+            shutil.move(item_path, os.path.join(path, folder, item))
+            moved = True
+            break
+
+    if not moved:
+        shutil.move(item_path, os.path.join(path, "Others", item))
